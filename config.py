522637ff0df88dbf50e9c631225b42e882df1d5b
gokul-gopan-k <gokul.gopan.k@gmail.com>
2022-05-27 18:30:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 class CONFIG:
     BATCH_SIZE= 32
     EPOCHS =50
-    data_path_csv = "../input/chinese-mnist-digit-recognizer/chineseMNIST.csv"
+    data_path_csv = "../input/chinese-mnist-digit-recognizer/ChineseDigits.csv"
     train_ratio = 0.75
     validation_ratio = 0.15
     test_ratio = 0.10
