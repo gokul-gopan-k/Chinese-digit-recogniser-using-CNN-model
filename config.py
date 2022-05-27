d57ca5911a1dd0333627944b48eea3a687ff2c26
gokul-gopan-k <gokul.gopan.k@gmail.com>
2022-05-27 18:25:46 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+class CONFIG:
+    BATCH_SIZE= 32
+    EPOCHS =50
+    data_path_csv = "../input/chinese-mnist-digit-recognizer/chineseMNIST.csv"
+    train_ratio = 0.75
+    validation_ratio = 0.15
+    test_ratio = 0.10
+
+    
